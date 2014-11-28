5f7120aa29235226d36081643d5f25a300b34b75
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-11-27 22:06:11 -0600

    starting uri builder

--- uri.go
+++ uri.go
@@ -1,38 +1,97 @@
 package otp
 
 import (
-	_ "bytes"
-	_ "text/template"
+	"bytes"
+	"encoding/base32"
+	"errors"
+	"strings"
+	"text/template"
 )
 
 // Defines a secret key per otpauth specifications. See https://code.google.com/p/google-authenticator/wiki/KeyUriFormat for more information.
 type KeyUri struct {
-	method  string // Defines the method used; must be `totp` or `hotp`.
-	label   string // Identifies the account the key is associated with.
-	secret  string // Base32 encoded secret key.
-	issuer  string // Identifies the service provider the key is associated with.
-	algo    string // The hash algorithm used. `SHA1`, `SHA256`, `SHA512`, or `MD5` are valid.
-	digits  int    // The length of the code.
-	counter int64  // The initial counter value. Applies only to `hotp`.
-	period  int64  // The number of second the code is valid for. Applies only to `totp`.
+	method string
+	label  string
+	secret string
+	issuer string
+	algo   string
+	digits int
+	param  int64
 }
 
-/* Returns the string representation of the KeyUri.
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func newKey(method, label, secret, issuer, algo string, digits int, param int64) (*KeyUri, error) {
+
+	// validate label
+	if strings.ContainsRune(label, '/') {
+		return &KeyUri{}, errors.New("Invalid `label`: Contains '/'.")
+	}
+
+	// validate secret
+	secret = strings.ToUpper(secret)
+	if _, err := base32.StdEncoding.DecodeString(secret); err != nil {
+		return &KeyUri{}, errors.New("Invalid `secret`: Does not decode from Base32.")
+	}
+
+	// validate issuer
+	if strings.ContainsRune(issuer, '/') {
+		return &KeyUri{}, errors.New("Invalid `issuer`: Contains '/'.")
+	}
+
+	// validate algo
+	algo = strings.ToUpper(algo)
+	validAlgos := []string{"SHA1", "SHA256", "SHA512", "MD5"}
+	if !stringInSlice(algo, validAlgos) {
+		return &KeyUri{}, errors.New("Invalid `algo`: Only SHA1, SHA256, SHA512, MD5 are supported.")
+	}
+
+	// validate digits
+	if !(digits == 6 || digits == 8) {
+		return &KeyUri{}, errors.New("Invalid `digits`: Value must be 6 or 8.")
+	}
+
+	// validate period
+	if method == "totp" && param < 0 {
+		return &KeyUri{}, errors.New("Invalid `param`: For totp, value must be positive.")
+	}
+
+	key := KeyUri{
+		method: method,
+		label:  label,
+		secret: secret,
+		issuer: issuer,
+		algo:   algo,
+		digits: digits,
+		param:  param,
+	}
+	return &key, nil
+}
+
+// Returns a TOTP KeyUri.
+func NewTOTP(label, secret, issuer, algo string, digits int, param int64) (*KeyUri, error) {
+	key, err := newKey("totp", label, secret, issuer, algo, digits, param)
+	return key, err
+}
+
+// Returns a HOTP KeyUri.
+func NewHOTP(label, secret, issuer, algo string, digits int, param int64) (*KeyUri, error) {
+	key, err := newKey("hotp", label, secret, issuer, algo, digits, param)
+	return key, err
+}
+
+// Returns the string representation of the KeyUri.
 func (k KeyUri) String() string {
 	markup := "otpauth://{{.method}}/{{.label}}?secret={{.secret}}"
-
-	if k.issuer != nil {
-		markup = markup + "&issuer={{.issuer}}"
-	}
-
-	if k.algo != nil {
-
-	} else {
-
-	}
-
 	tmpl, _ := template.New("uri").Parse(markup)
 	var uri bytes.Buffer
 	tmpl.Execute(&uri, k)
 	return uri.String()
-}*/
+}
