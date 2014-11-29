9b14bb2c0a24c0694c473eded01d0a141beb00da
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-11-28 21:59:25 -0600

    valdition tests

--- key_test.go
+++ key_test.go
@@ -58,6 +58,64 @@ func TestBadSecret(t *testing.T) {
 	}
 }
 
+func TestBadIssuer(t *testing.T) {
+	key := Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issu/er",
+	}
+	v, _ := key.IsValid()
+	if v == true {
+		t.Fail()
+	}
+}
+
+func TestBadAlgo(t *testing.T) {
+	key := Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   "blowfish",
+	}
+	v, _ := key.IsValid()
+	if v == true {
+		t.Fail()
+	}
+}
+
+func TestBadDigits(t *testing.T) {
+	key := Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   "SHA1",
+		Digits: 99,
+	}
+	v, _ := key.IsValid()
+	if v == true {
+		t.Fail()
+	}
+}
+
+func TestBadPeriod(t *testing.T) {
+	key := Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   "SHA1",
+		Digits: 6,
+		Period: -42,
+	}
+	v, _ := key.IsValid()
+	if v == true {
+		t.Fail()
+	}
+}
+
 func TestTotpString(t *testing.T) {
 	key, _ := NewTotp(
 		"label",
