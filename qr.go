dfbccbae6e0185096216db2512ab4f3edd21dc97
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-11-29 22:39:37 -0600

    fixed prototype

--- qr.go
+++ qr.go
@@ -4,6 +4,7 @@ import (
 	"code.google.com/p/rsc/qr"
 )
 
-func (k Key) QrCode() (*qr.Code, err) {
-	return qr.Encode(k.ToURI(), qr.H)
+func (k Key) QrCode() (*qr.Code, error) {
+	code, err := qr.Encode(k.ToURI(), qr.H)
+	return code, err
 }
