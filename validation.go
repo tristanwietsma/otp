f5670c89e5959b07d527f6751e2d54a2060abd54
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-11-30 22:28:57 -0600

    refactor keys a bit

---
+++ validation.go
@@ -0,0 +1,105 @@
+package otp
+
+import (
+	"encoding/base32"
+	"strings"
+)
+
+func (k Key) hasValidMethod() error {
+	if !stringInSlice(k.Method, METHODS) {
+		return KeyError{"Method", "Invalid value"}
+	}
+	return nil
+}
+
+func (k Key) hasValidLabel() error {
+	if len(k.Label) == 0 {
+		return KeyError{"Label", "Missing value"}
+	}
+
+	if strings.ContainsRune(k.Label, '/') {
+		return KeyError{"Label", "Contains '/'"}
+	}
+
+	return nil
+}
+
+func (k Key) hasValidSecret() error {
+	if len(k.Secret) == 0 {
+		return KeyError{"Secret", "Missing value"}
+	}
+
+	if _, err := base32.StdEncoding.DecodeString(k.Secret); err != nil {
+		return KeyError{"Secret", "Invalid Base32"}
+	}
+
+	return nil
+}
+
+func (k Key) hasValidIssuer() error {
+	if strings.ContainsRune(k.Issuer, '/') {
+		return KeyError{"Issuer", "Contains '/'"}
+	}
+	return nil
+}
+
+func (k Key) hasValidAlgo() error {
+	if !hashInSlice(k.Algo, HASHES) {
+		return KeyError{"Algo", "Invalid hashing algorithm"}
+	}
+	return nil
+}
+
+func (k Key) hasValidDigits() error {
+	if !(k.Digits == 6 || k.Digits == 8) {
+		return KeyError{"Digits", "Not equal to 6 or 8"}
+	}
+	return nil
+}
+
+func (k Key) hasValidPeriod() error {
+	if k.Method == "totp" && k.Period < 1 {
+		return KeyError{"Period", "Negative value"}
+	}
+	return nil
+}
+
+func (k Key) IsValid() (bool, error) {
+
+	// check method
+	if err := k.hasValidMethod(); err != nil {
+		return false, err
+	}
+
+	//check label
+	if err := k.hasValidLabel(); err != nil {
+		return false, err
+	}
+
+	// check secret
+	if err := k.hasValidSecret(); err != nil {
+		return false, err
+	}
+
+	// check issuer
+	if err := k.hasValidIssuer(); err != nil {
+		return false, err
+	}
+
+	// check algo
+	if err := k.hasValidAlgo(); err != nil {
+		return false, err
+	}
+
+	// check digits
+	if err := k.hasValidDigits(); err != nil {
+		return false, err
+	}
+
+	// check period
+	if err := k.hasValidPeriod(); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
