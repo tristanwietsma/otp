918a7ce9e19ab4716a80f1a7845130f65785cc5b
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-12-01 22:24:17 -0600

    break up validation test file

--- key_test.go
+++ key_test.go
@@ -1,7 +1,6 @@
 package otp
 
 import (
-	"code.google.com/p/go.crypto/md4"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
@@ -16,56 +15,6 @@ func TestKeyError(t *testing.T) {
 	}
 	if err.Error() != "KeyError - param - msg" {
 		t.Fail()
-	}
-}
-
-var BadKeys = []Key{
-	Key{
-		Method: "crypto!",
-	},
-	Key{
-		Method: "totp",
-	},
-	Key{
-		Method: "totp",
-		Label:  "t@w",
-	},
-	Key{
-		Method: "totp",
-		Label:  "t@w",
-		Secret: "abc123",
-	},
-	Key{
-		Method: "totp",
-		Label:  "t@w",
-		Secret: "MFRGGZDFMZTWQ2LK",
-		Issuer: "issuer",
-		Algo:   md4.New,
-	},
-	Key{
-		Method: "totp",
-		Label:  "t@w",
-		Secret: "MFRGGZDFMZTWQ2LK",
-		Issuer: "issuer",
-		Algo:   sha1.New,
-		Digits: 99,
-	},
-	Key{
-		Method: "totp",
-		Label:  "t@w",
-		Secret: "MFRGGZDFMZTWQ2LK",
-		Issuer: "issuer",
-		Algo:   sha1.New,
-		Digits: 6,
-		Period: -42,
-	},
-}
-
-func TestBadKeys(t *testing.T) {
-	for _, k := range BadKeys {
-		if err := k.Validate; err == nil {
-			t.Errorf("Bad Key didn't produce error on Validate(): %v", k)
-		}
 	}
 }
 
---
+++ validation_test.go
@@ -0,0 +1,57 @@
+package otp
+
+import (
+	"code.google.com/p/go.crypto/md4"
+	"crypto/sha1"
+	"testing"
+)
+
+var BadKeys = []Key{
+	Key{
+		Method: "crypto!",
+	},
+	Key{
+		Method: "totp",
+	},
+	Key{
+		Method: "totp",
+		Label:  "t@w",
+	},
+	Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "abc123",
+	},
+	Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   md4.New,
+	},
+	Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   sha1.New,
+		Digits: 99,
+	},
+	Key{
+		Method: "totp",
+		Label:  "t@w",
+		Secret: "MFRGGZDFMZTWQ2LK",
+		Issuer: "issuer",
+		Algo:   sha1.New,
+		Digits: 6,
+		Period: -42,
+	},
+}
+
+func TestBadKeys(t *testing.T) {
+	for _, k := range BadKeys {
+		if err := k.Validate; err == nil {
+			t.Errorf("Bad Key didn't produce error on Validate(): %v", k)
+		}
+	}
+}
