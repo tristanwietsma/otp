10d22fd61a494c230531641cec6e402a8bc59070
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-12-03 21:42:00 -0600

    key docs

--- key.go
+++ key.go
@@ -12,19 +12,19 @@ var methods = []string{"totp", "hotp"}
 // Supported hash algorithms.
 var Hashes = []Hash{sha1.New, sha256.New, sha512.New, md5.New}
 
-// Key defines a set of parameters required for code generation as well as metadata.
+// Key represents a one-time password. It supports time-based (totp) and HMAC-based (hotp) approaches.
 type Key struct {
-	Method  string // Initialization method. Acceptable values are either 'totp' or 'hotp' for time-based or counter-based, respectively.
-	Label   string // Label for the key.
-	Secret  string // String representation of base32 encoded integer.
-	Issuer  string // The issuer of the key.
-	Algo    Hash   // The hash algorithm used in the HMAC. SHA1, SHA256, SHA512, and MD5 are supported.
-	Digits  int    // The length of the code. 6 or 8 are acceptable.
-	Period  int    // The number of seconds the code is valid for. Applies only to 'totp'.
-	Counter int    // The initial counter value. Applies only to 'hotp'.
+	Method  string // Initialization method. Either 'totp' or 'hotp'.
+	Label   string // Descriptive label..
+	Secret  string // Base32-encoded secret key.
+	Issuer  string // Key issuer.
+	Algo    Hash   // Hash algorithm. See Hashes.
+	Digits  int    // Length of the code. Either 6 or 8.
+	Period  int    // Seconds code is valid for. Applies only to 'totp'.
+	Counter int    // Initial counter value. Applies only to 'hotp'.
 }
 
-// GetCode accepts an initialization value and returns a corresponding code.
+// GetCode returns a one-time password code an initial value..
 func (k Key) GetCode(iv int64) (string, error) {
 	code, err := GetCode(k.Secret, iv, k.Algo, k.Digits)
 	return code, err
